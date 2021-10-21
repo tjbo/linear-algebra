e93058c0520d85a1784b62c467aef27c000f9c15
tjbo <tom@prototypable.ca>
2021-10-21 13:24:58 -0500

    add vector with complex numbers

---
+++ vector-with-complex-numbers.py
@@ -0,0 +1,17 @@
+# vector with complex numbers
+# | 1 3i | - complex number, imaginery is 3
+# |  -2i | - complex number (real # is 0), imaginery is 2
+# |   4  | - real number
+# |  -5  | - real number
+
+
+# dot product
+# | 1 3i | * | 6 2i |
+# |  -2i |   |  8   |
+# |   4  |   |  3i  |
+# |  -5  |   |  -5  |
+
+
+# = (1 3i)(6 2i) + -16i + 12i + 25
+# = 6 + 2i + 18i - 6 - 16i + 12i + 25
+# = 25 + 16i
